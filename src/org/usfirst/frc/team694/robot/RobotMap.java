a53f0c63b268f57731de6ca9622fa488d29904f8
adrisj7 <adrisj7@gmail.com>
2017-01-10 18:28:06 -0500

    Add Michael1 Code for pulling to different computer

---
+++ src/org/usfirst/frc/team694/robot/RobotMap.java
@@ -0,0 +1,18 @@
+package org.usfirst.frc.team694.robot;
+
+/**
+ * The RobotMap is a mapping from the ports sensors and actuators are wired into
+ * to a variable name. This provides flexibility changing wiring, makes checking
+ * the wiring easier and significantly reduces the number of magic numbers
+ * floating around.
+ */
+public class RobotMap {
+
+	public static final int GAMEPAD = 0;
+	
+	public static final int LEFT_MOTOR = 1;
+	public static final int RIGHT_MOTOR = 2;
+
+	public static final int BOTTOM_ROLLER = 3;
+	public static final int TOP_ROLLER = 4;
+}
---
+++ src/org/usfirst/frc/team694/robot/subsystems/Thomas.java
@@ -0,0 +1,28 @@
+package org.usfirst.frc.team694.robot.subsystems;
+
+import org.usfirst.frc.team694.robot.RobotMap;
+
+import edu.wpi.first.wpilibj.TalonSRX;
+import edu.wpi.first.wpilibj.command.Subsystem;
+
+public class Thomas extends Subsystem {
+
+	TalonSRX leftMotor;
+	TalonSRX rightMotor;
+	
+	public Thomas(){
+		leftMotor = new TalonSRX(RobotMap.LEFT_MOTOR);
+		rightMotor = new TalonSRX(RobotMap.RIGHT_MOTOR);
+	}
+
+    // Put methods for controlling this subsystem
+    // here. Call these from Commands.
+	public void tankDrive (float left, float right){
+		leftMotor.set(left); //Did you just assume the orientation of the Robot?
+		rightMotor.set(right);
+	}
+    public void initDefaultCommand() {
+        // Set the default command for a subsystem here.
+    }
+}
+
